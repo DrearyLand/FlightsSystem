1b89681cffe09e2e1f7b062a9b85cd642f946293
Ezzat Alzahouri Campos <A01710709@tec.mx>
2022-11-28 17:23:00 -0600

    Adding flights available with different destinations

--- ProyectoIntegrador.cpp
+++ ProyectoIntegrador.cpp
@@ -63,27 +63,258 @@ class registration{
 
             switch(choice){
                 case 1:{
-                    cout<<"______________Queretaro______________\n"<<endl;
+                    cout<<"______________Flights to Queretaro______________\n"<<endl;
                     cout<<"Following are the flights \n"<<endl;
 
                     cout<<"1. Queretaro - 498";
-                    cout<<"\t16-12-2022 08:00AM 10hrs $1,500"<<endl;
-                    cout<<"1. Queretaro - 658";
-                    cout<<"\t16-12-2022 16:00AM 12hrs $1,000"<<endl;
-                    cout<<"1. Queretaro - 220";
-                    cout<<"\t16-12-2022 22:00AM 11hrs $1,200"<<endl;
+                    cout<<"\t16-12-2022 08:00AM 1hrs $1,699"<<endl;
+                    cout<<"2. Queretaro - 658";
+                    cout<<"\t16-12-2022 16:00AM 4hrs $1,199"<<endl;
+                    cout<<"3. Queretaro - 220";
+                    cout<<"\t16-12-2022 22:00AM 2hrs $1,399"<<endl;
 
                     cout<<"\nSelect what flight do you want: ";
                     cin>>choice;
 
                     if(choice==1){
-                        charges=1500;
+                        charges=1699;
                         cout<<"\nPurchase Compleate."<<endl;
                         cout<<"You can get your ticket in the main menu. "<<endl;
-
                     }
+                    else if(choice==2){
+                        charges=1199;
+                        cout<<"\nPurchase Compleate."<<endl;
+                        cout<<"You can get your ticket in the main menu. "<<endl;
+                    }
+                    else if(choice==3){
+                        charges=1399;
+                        cout<<"\nPurchase Compleate."<<endl;
+                        cout<<"You can get your ticket in the main menu. "<<endl;
+                    }
+                    else{
+                        cout<<"Invalid input, shifting to the previous menu"<<endl;
+                        flights();
+                    }
+
+                    cout<<"Press any key to go back to the main menu: "<<endl;
+                    cin>>back;
+
+                    if(back==1){mainMenu();}
+                    else{mainMenu();}
+
+                }
+                
+                case 2:{
+                    cout<<"______________Flights to Monterrey______________\n"<<endl;
+                    cout<<"Following are the flights \n"<<endl;
+
+                    cout<<"1. Monterrey - 267";
+                    cout<<"\t13-12-2022 05:00AM 2hrs $1,999"<<endl;
+                    cout<<"2. Monterrey - 401";
+                    cout<<"\t13-12-2022 13:00AM 5hrs $1,299"<<endl;
+                    cout<<"3. Monterrey - 463";
+                    cout<<"\t13-12-2022 23:00AM 3hrs $1,499"<<endl;
+
+                    cout<<"\nSelect what flight do you want: ";
+                    cin>>choice;
+
+                    if(choice==1){
+                        charges=1999;
+                        cout<<"\nPurchase Compleate."<<endl;
+                        cout<<"You can get your ticket in the main menu. "<<endl;
+                    }
+                    else if(choice==2){
+                        charges=1299;
+                        cout<<"\nPurchase Compleate."<<endl;
+                        cout<<"You can get your ticket in the main menu. "<<endl;
+                    }
+                    else if(choice==3){
+                        charges=1499;
+                        cout<<"\nPurchase Compleate."<<endl;
+                        cout<<"You can get your ticket in the main menu. "<<endl;
+                    }
+                    else{
+                        cout<<"Invalid input, shifting to the previous menu"<<endl;
+                        flights();
+                    }
+
+                    cout<<"Press any key to go back to the main menu: "<<endl;
+                    cin>>back;
+
+                    if(back==1){mainMenu();}
+                    else{mainMenu();}
+                }
+
+                case 3:{
+                    cout<<"______________Flights to Guadalajara______________\n"<<endl;
+                    cout<<"Following are the flights \n"<<endl;
+
+                    cout<<"1. Guadalajara - 653";
+                    cout<<"\t03-12-2022 05:00AM 2hrs $1,999"<<endl;
+                    cout<<"2. Guadalajara - 516";
+                    cout<<"\t07-12-2022 13:00AM 5hrs $1,299"<<endl;
+                    cout<<"3. Guadalajara - 158";
+                    cout<<"\t02-12-2022 23:00AM 3hrs $1,499"<<endl;
+
+                    cout<<"\nSelect what flight do you want: ";
+                    cin>>choice;
+
+                    if(choice==1){
+                        charges=1999;
+                        cout<<"\nPurchase Compleate."<<endl;
+                        cout<<"You can get your ticket in the main menu. "<<endl;
+                    }
+                    else if(choice==2){
+                        charges=1299;
+                        cout<<"\nPurchase Compleate."<<endl;
+                        cout<<"You can get your ticket in the main menu. "<<endl;
+                    }
+                    else if(choice==3){
+                        charges=1499;
+                        cout<<"\nPurchase Compleate."<<endl;
+                        cout<<"You can get your ticket in the main menu. "<<endl;
+                    }
+                    else{
+                        cout<<"Invalid input, shifting to the previous menu"<<endl;
+                        flights();
+                    }
+
+                    cout<<"Press any key to go back to the main menu: "<<endl;
+                    cin>>back;
+
+                    if(back==1){mainMenu();}
+                    else{mainMenu();}
+                }
+
+                case 4:{
+                    cout<<"______________Flights to Toronto______________\n"<<endl;
+                    cout<<"Following are the flights \n"<<endl;
+
+                    cout<<"1. Toronto - 238";
+                    cout<<"\t22-12-2022 11:00AM 4hrs $3,999"<<endl;
+                    cout<<"2. Toronto - 184";
+                    cout<<"\t12-12-2022 17:00AM 9hrs $2,499"<<endl;
+                    cout<<"3. Toronto - 360";
+                    cout<<"\t15-12-2022 19:00AM 6hrs $3,199"<<endl;
+
+                    cout<<"\nSelect what flight do you want: ";
+                    cin>>choice;
+
+                    if(choice==1){
+                        charges=3999;
+                        cout<<"\nPurchase Compleate."<<endl;
+                        cout<<"You can get your ticket in the main menu. "<<endl;
+                    }
+                    else if(choice==2){
+                        charges=2499;
+                        cout<<"\nPurchase Compleate."<<endl;
+                        cout<<"You can get your ticket in the main menu. "<<endl;
+                    }
+                    else if(choice==3){
+                        charges=3199;
+                        cout<<"\nPurchase Compleate."<<endl;
+                        cout<<"You can get your ticket in the main menu. "<<endl;
+                    }
+                    else{
+                        cout<<"Invalid input, shifting to the previous menu"<<endl;
+                        flights();
+                    }
+
+                    cout<<"Press any key to go back to the main menu: "<<endl;
+                    cin>>back;
+
+                    if(back==1){mainMenu();}
+                    else{mainMenu();}
+                }
+
+                case 5:{
+                    cout<<"______________Flights to Boston______________\n"<<endl;
+                    cout<<"Following are the flights \n"<<endl;
+
+                    cout<<"1. Boston - 384";
+                    cout<<"\t21-12-2022 12:00AM 4hrs $2,999"<<endl;
+                    cout<<"2. Boston - 830";
+                    cout<<"\t19-12-2022 15:00AM 8hrs $1,699"<<endl;
+                    cout<<"3. Boston - 423";
+                    cout<<"\t27-12-2022 20:00AM 6hrs $2,199"<<endl;
+
+                    cout<<"\nSelect what flight do you want: ";
+                    cin>>choice;
+
+                    if(choice==1){
+                        charges=2999;
+                        cout<<"\nPurchase Compleate."<<endl;
+                        cout<<"You can get your ticket in the main menu. "<<endl;
+                    }
+                    else if(choice==2){
+                        charges=1699;
+                        cout<<"\nPurchase Compleate."<<endl;
+                        cout<<"You can get your ticket in the main menu. "<<endl;
+                    }
+                    else if(choice==3){
+                        charges=2199;
+                        cout<<"\nPurchase Compleate."<<endl;
+                        cout<<"You can get your ticket in the main menu. "<<endl;
+                    }
+                    else{
+                        cout<<"Invalid input, shifting to the previous menu"<<endl;
+                        flights();
+                    }
+
+                    cout<<"Press any key to go back to the main menu: "<<endl;
+                    cin>>back;
+
+                    if(back==1){mainMenu();}
+                    else{mainMenu();}
+                }
+
+                case 6: {
+                    cout<<"______________Flights to San Fransisco______________\n"<<endl;
+                    cout<<"Following are the flights \n"<<endl;
+
+                    cout<<"1. San Fransisco - 258";
+                    cout<<"\t18-12-2022 07:00AM 3hrs $2,599"<<endl;
+                    cout<<"2. San Fransisco - 377";
+                    cout<<"\t16-12-2022 13:00AM 7hrs $1,599"<<endl;
+                    cout<<"3. San Fransisco - 542";
+                    cout<<"\t30-12-2022 22:00AM 5hrs $2,199"<<endl;
+
+                    cout<<"\nSelect what flight do you want: ";
+                    cin>>choice;
+
+                    if(choice==1){
+                        charges=2599;
+                        cout<<"\nPurchase Compleate."<<endl;
+                        cout<<"You can get your ticket in the main menu. "<<endl;
+                    }
+                    else if(choice==2){
+                        charges=1599;
+                        cout<<"\nPurchase Compleate."<<endl;
+                        cout<<"You can get your ticket in the main menu. "<<endl;
+                    }
+                    else if(choice==3){
+                        charges=2199;
+                        cout<<"\nPurchase Compleate."<<endl;
+                        cout<<"You can get your ticket in the main menu. "<<endl;
+                    }
+                    else{
+                        cout<<"Invalid input, shifting to the previous menu"<<endl;
+                        flights();
+                    }
+
+                    cout<<"Press any key to go back to the main menu: "<<endl;
+                    cin>>back;
+
+                    if(back==1){mainMenu();}
+                    else{mainMenu();}
+                }
+                default:{
+                    cout<<"Invalid Input, Shifting to the main menu. "<<endl;
+                    mainMenu();
+                    break;
                 }
             }
+
         }
 }
 
@@ -120,12 +351,8 @@ void mainMenu(){
             cout<<"Press any to go back to the main menu ";
             cin>>back;
 
-            if(back==1){
-                mainMenu();
-            }
-            else{
-                mainMenu();
-            }
+            if(back==1){mainMenu();}
+            else{mainMenu();}
             break;
         }
         case 2:{
@@ -144,16 +371,10 @@ void mainMenu(){
                 t.display();
                 cout<<"Press any to go back to the main menu ";
                 cin>>back;
-                if(back==1){
-                    mainMenu();
-                }
-                else{
-                    mainMenu();
-                }
+                if(back==1){mainMenu();}
+                else{mainMenu();}
             }
-            else{
-                mainMenu();
-            }          
+            else{mainMenu();}          
             break;
         }
         case 4:{
